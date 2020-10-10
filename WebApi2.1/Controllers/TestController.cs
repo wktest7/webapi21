b6bd17a865e87913f49e5d0b2c7bff781319eee1
wktest7 <wk27qwerty27@gmail.com>
2020-10-11 00:30:32 +0200

    Add project files.

---
+++ WebApi2.1/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("guid")]
+        public IActionResult Get()
+        {
+            return Ok(Guid.NewGuid().ToString());
+        }
+    }
+}
